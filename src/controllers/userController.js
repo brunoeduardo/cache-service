a575a5c1287cd4c46a202200e8d74024ba20ddbf
Bruno Eduardo <bruno@beduardo.com>
2019-11-24 15:44:19 -0300

    add redis to get user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,10 +8,10 @@ var client = redis.createClient();
 exports.get = (req, res, next) => {
     client.get('alluser', function (err, reply) {
         if (reply) {
-            console.log('redis');
-            res.send(reply)
+            console.log('Redis get AllUsers');
+            res.send(JSON.parse(reply));
         } else {
-            console.log('db');
+            console.log('DB get AllUsers');
 
         UserRepository.getAll()
             .then((user) => {
@@ -24,11 +24,21 @@ exports.get = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
+    client.get("user" + req.params.id, function (err, reply) {
+        if(reply) {
+            console.log(`Redis get User`);
+            res.send(JSON.parse(reply));
+        } else {
+            console.log(`DB get User`);
 
-    UserRepository.getById(req.params.id)
-        .then((user) => {
-            res.status(200).send(user);
-        }).catch(err => res.status(500).send(err))
+            UserRepository.getById(req.params.id)
+                .then((user) => {
+                    client.set("user" + req.params.id, JSON.stringify(user));
+                    client.expire("user" + req.params.id, 20);
+                    res.status(200).send(user);
+            }).catch(err => res.status(500).send(err))
+        }
+    });
 };
 
 exports.post = (req, res, next) => {
